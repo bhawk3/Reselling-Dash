6192135f8fdc3126b494687087d74b852ad99c41
Brendan Hawk <brendanhawk03@aol.com>
2026-04-06 07:51:07 -0400

    Basic components with Inventory/Dashboard set up without styling

---
+++ src/components/InventoryData.jsx
@@ -0,0 +1,15 @@
+//This is going to contain all of the <tr> data
+export default function InventoryData() {
+	return (
+		<>
+			<tr>
+				<td>bruh</td>
+				<td>bruh2</td>
+				<td>bruh3</td>
+				<td>bruh4</td>
+				<td>bruh5</td>
+			</tr>
+			<tr>test</tr>
+		</>
+	);
+}
--- src/components/InventoryManager.jsx
+++ src/components/InventoryManager.jsx
@@ -1,5 +1,25 @@
 //This is going to contain the table head / tbody and then the individual table rows will get placed in their own component
+import InventoryData from "./InventoryData";
 
 export default function InventoryManager() {
-	return <></>;
+	return (
+		<>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Quantity</th>
+						<th>Status</th>
+						<th>Bought Price</th>
+						<th>Sold Price</th>
+						<th>Profit</th>
+						<th>Sourcing Location</th>
+					</tr>
+				</thead>
+				<tbody>
+					<InventoryData />
+				</tbody>
+			</table>
+		</>
+	);
 }
